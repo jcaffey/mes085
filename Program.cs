4d3a792011346c9eb5d0fdf2ce11844e9d36c837
Josh Caffey <jcaffey@gmail.com>
2020-01-30 16:12:05 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Mes.Gateway;
+
+namespace Mes085
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GatewaySettings settings = new GatewaySettings();
+            settings.setCredentials("CHANGE", "ME")
+            .setVerbose(true)
+            .setHostUrl(GatewaySettings.URL_CERT);
+            Gateway gateway = new Gateway(settings);
+
+            // run verify to get 085
+            GatewayRequest request = new GatewayRequest(GatewayRequest.TransactionType.VERIFY);
+            request.setCardData("4111111111111111", "1221");
+            request.setAmount("0.00");
+            GatewayResponse response = gateway.run(request);
+
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("Error code with VERIFY (A) and amount 0.00:");
+            Console.WriteLine(response.getErrorCode());
+            Console.WriteLine("--------------------------------------------------------");
+
+            // sale at 0.85
+            request = new GatewayRequest(GatewayRequest.TransactionType.SALE);
+            request.setCardData("4111111111111111", "1221");
+            request.setAmount("0.85");
+            response = gateway.run(request);
+            
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("Error code with SALE (D) and amount 0.85:");
+            Console.WriteLine(response.getErrorCode());
+            Console.WriteLine("--------------------------------------------------------");
+
+            // Make refund request with transaction id of previous sale...
+            // No 085 ... error code is 000
+            request = new GatewayRequest(GatewayRequest.TransactionType.REFUND);
+            request.setTranId(response.getTransactionId());
+            response = gateway.run(request);
+
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("Error code with REFUND (U):");
+            Console.WriteLine(response.getErrorCode());
+            Console.WriteLine("--------------------------------------------------------");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# sample output
+
+Response: (HTTP 200 - 1625ms) transaction_id=09d2560e9f7c39c3b72d69accdde4e07&error_code=085&auth_response_text=Card Ok&avs_result=0&auth_code=T5776H
+--------------------------------------------------------
+Error code with VERIFY (A) and amount 0.00:
+085
+--------------------------------------------------------
+Response: (HTTP 200 - 1332ms) transaction_id=2dd15ec8ee023e678107bff5a66ff361&error_code=000&auth_response_text=Approval T5777H&avs_result=0&auth_code=T5777H
+--------------------------------------------------------
+Error code with SALE (D) and amount 0.85:
+000
+--------------------------------------------------------
+Response: (HTTP 200 - 1664ms) transaction_id=47c97de7c295443082c5039d9d0495aa&error_code=000&auth_response_text=Refund Request Accepted - Void
+--------------------------------------------------------
+Error code with REFUND (U):
+000
+--------------------------------------------------------
